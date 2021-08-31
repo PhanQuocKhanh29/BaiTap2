1d7e481ad406ef0da4fde087d4f04b3d6852a20d
PhanQuocKhanh29 <88721369+PhanQuocKhanh29@users.noreply.github.com>
2021-08-31 09:28:53 +0700

    FirstCommit

---
+++ BaiTap2/Controllers/DemoBT2Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap2.Controllers
+{
+    public class DemoBT2Controller : Controller
+    {
+        // GET: DemoBT2
+        public ActionResult GPT()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GPT(string soA, string soB)
+        {
+            double so1 = Convert.ToDouble(soA);
+            double so2 = Convert.ToDouble(soB);
+            double ketqua = -so2 / so1;
+            ViewBag.Giaipt = ketqua;
+            return View();
+        }
+    }
+}
---
+++ BaiTap2/Models/BaiTap2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap2.Models
+{
+    public class BaiTap2
+    {
+        public double GPT(double a, double b)
+        {
+            double GPT = 0;
+            GPT = -b / a;
+            return GPT;
+        }
+    }
+}
