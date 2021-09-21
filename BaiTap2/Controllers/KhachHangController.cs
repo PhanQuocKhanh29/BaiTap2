12ba7982c179314e4933012ecf352d49c0dcbda1
PhanQuocKhanh29 <88721369+PhanQuocKhanh29@users.noreply.github.com>
2021-09-21 09:08:05 +0700

    commit

---
+++ BaiTap2/Controllers/KhachHangController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System
+
+namespace BaiTap2.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
